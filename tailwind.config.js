12b95189e21399e80f8f8a6976c6dd1a76bf0941
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-08-10 19:21:43 +0300

    chore: setup react project with tailwind and typescript

---
+++ tailwind.config.js
@@ -0,0 +1,59 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      display: ['Open Sans', 'sans-serif'],
+      body: ['Open Sans', 'sans-serif'],
+    },
+    extend: {
+      fontSize: {
+        12: '12px',
+        14: '14px',
+        16: '16px',
+      },
+      backgroundColor: {
+        'main-bg': '#FFFFFF',
+        'main-dark-bg': '#FFFBF7',
+        'secondary-dark-bg': '#33373E',
+        'light-gray': '#FFFBF7',
+        'half-transparent': 'rgba(0, 0, 0, 0.5)',
+      },
+      borderWidth: {
+        1: '1px',
+      },
+      borderColor: {
+        colors: '#F0F0F0',
+      },
+      fontColor : {
+          black: '#222223',
+          darkGrey: '#464646',
+          grey: '#4C4C4C',
+          lightGrey: '#A3A3A3'
+
+      },
+
+      width: {
+        400: '400px',
+        760: '760px',
+        780: '780px',
+        800: '800px',
+        1000: '1000px',
+        1200: '1200px',
+        1400: '1400px',
+      },
+      height: {
+        80: '80px',
+      },
+      minHeight: {
+        590: '590px',
+      },
+      backgroundImage: {
+        'hero-pattern':
+          "url('https://demos.wrappixel.com/premium-admin-templates/react/flexy-react/main/static/media/welcome-bg-2x-svg.25338f53.svg')",
+      },
+    },
+  },
+  plugins: [],
+};
