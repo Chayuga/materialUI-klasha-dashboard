3c401072e69534f5fc20d90d52df6eb591080c92
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-08-13 19:33:05 +0300

    build: build react Line chart

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,11 +3,13 @@ import { TbCurrencyNaira } from 'react-icons/tb';
 import { earningData, SparklineAreaData } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 import GraphCard from '../components/GraphCard/GraphCard';
+import AreaChart from '../components/Charts/AreaChart';
 
 const Dashboard = () => {
   return (
     <div>
       <GraphCard />
+      <AreaChart />
     </div>
   );
 };
