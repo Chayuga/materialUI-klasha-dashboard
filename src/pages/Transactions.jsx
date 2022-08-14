2ffa784290274ebceb7503ef80c057cc674a7cee
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-08-14 13:53:07 +0300

    feature: add transactions data placeholder

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,10 +1,10 @@
+import { Typography } from '@mui/material';
 import React from 'react';
-import { DisplayTable } from '../components';
 
 const Transactions = () => {
   return (
     <div>
-      <DisplayTable />
+      <Typography>Transactions table</Typography>
     </div>
   );
 };
