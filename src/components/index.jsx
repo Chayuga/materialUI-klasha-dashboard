b68186bfd39a8ef121cac049c358c83dd83ed218
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-08-15 13:41:19 +0300

    del: clear all unused codes

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,4 +2,3 @@ export { default as Header } from './Header';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Sidebar } from './Sidebar/Sidebar';
 export { default as Footer } from './Footer';
-export { default as AreaChart } from './Charts/AreaChart';
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,7 @@
 import React from 'react';
-import { TbCurrencyNaira } from 'react-icons/tb';
-import { earningData, SparklineAreaData } from '../data/dummy';
-import { useStateContext } from '../contexts/ContextProvider';
-import GraphCard from '../components/GraphCard/GraphCard';
-import AreaChart from '../components/Charts/AreaChart';
 
 const Dashboard = () => {
-  return (
-    <div>
-      <GraphCard />
-      <AreaChart />
-    </div>
-  );
+  return <div>Dashboard</div>;
 };
 
 export default Dashboard;
